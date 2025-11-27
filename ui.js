a16f1dd87c407a3b0ca4bd9243f6ee17294ff81b
korchaginkirill911-ui <korchaginkirill911@gmail.com>
2025-11-27 22:10:41 +0300

    Delete ui.js

--- ui.js
+++
@@ -1,9 +0,0 @@
-// В ui.js замените функцию updateStats на эту:
-export const updateStats = () => {
-    const { total, completed } = getTasksStats();
-    const pending = total - completed;
-    
-    totalTasksEl.textContent = total;
-    completedTasksEl.textContent = completed;
-    document.getElementById('pendingTasks').textContent = pending;
-};
